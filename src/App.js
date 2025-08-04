4c577ec01a79f08a4d65e6cb7ab4f3c9eee305d8
Leximir <aleksa.stankovic@proton.me>
2025-08-04 14:31:18 +0200

    Homework done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import siteLogo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const name = 'Aleksa';
+const paragraphColor = 'purple';
+
+let date = new Date();
+let currentHours = date.getHours();
+console.log(currentHours);
+
+const isNight = currentHours >= 20 || currentHours <= 7;
+
+function App(){
+
+    const mainDivStyle = {
+        backgroundColor: isNight ? 'darkblue' : 'white',
+    }
+
+    return (
+        <div className='mainDiv' style={mainDivStyle}>
+            <p className='paragraph'>Pozdrav ja se zovem { name }</p>
+            <img src={ siteLogo } alt=""/>
+            <p style={{ color: paragraphColor}}>Ovo je moj prvi React App</p>
+        </div>
+    );
 }
 
 export default App;
